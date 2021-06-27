a92049f9ef45bcd770b86b74978516a83c57a175
Yuxi Wang <wyxr18@sina.com>
2021-06-27 06:03:50 -0400

    update batch files

--- Art/replaceabletexturesHD/commandbuttonsdisabled/displayandextract.bat
+++ Art/replaceabletexturesHD/commandbuttonsdisabled/displayandextract.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 for /d %%x in (*) do (
     if not "%%x" == "all" (
-    	for /f %%y in ('dir /b "%%x"') do (
+    	for /f "delims=" %%y in ('dir /b "%%x"') do (
 		echo replaceabletextures\\commandbuttons\\%%y
 		xcopy "%%x\%%y" "all\" > nul
     	)
